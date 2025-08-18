39d4b79599d0766e129b50a238190eb719c459e3
yohannesjho <jhonians@outlook.com>
2025-08-18 12:12:30 +0300

    Initial project setup: Next.js + shadcn + Prisma + Resend + Gemini config

---
+++ lib/gemini.ts
@@ -0,0 +1,15 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
