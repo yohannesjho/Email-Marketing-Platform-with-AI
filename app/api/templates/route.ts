c0699aead0b1dcc3d9f8fe6cc180a20879190252
yohannesjho <jhonians@outlook.com>
2025-09-02 22:52:33 +0300

    create a template

--- app/api/templates/route.ts
+++ app/api/templates/route.ts
@@ -22,3 +22,35 @@ export async function GET() {
         return NextResponse.json({message: "Internal Server Error"}, { status: 500})
     }
 }
+
+export async function POST(req: Request) {
+    try {
+        const user = await getUserFromToken();
+
+        if(!user) {
+            return NextResponse.json({message: "Unauthorized"}, { status: 401})
+        }
+
+        const body = await req.json();
+
+        const { name, subject, body: templateBody } = body
+
+        if( !name || !subject || !templateBody ) {
+            return NextResponse.json({message: "Missing fields"}, { status: 400})
+        }
+
+        const template = await prisma.template.create({
+            data: {
+                userId: user.id,
+                name,
+                subject,
+                body: templateBody,
+                 
+            }
+        })
+
+        return NextResponse.json(template, { status: 201})
+    } catch (error) {
+        return NextResponse.json({message: "Internal Server Error"}, { status: 500})
+    }
+}
